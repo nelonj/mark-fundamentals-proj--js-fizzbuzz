0d0f4871d083e553731fae938a9ea0839d52448b
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-01 17:08:50 +0000

    modified FizzBuzz to run with arrow functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,47 +1,78 @@
-function fizzbuzz(endNumber) {
-  // Note: an array map would be preferred here, but we'll look at maps later
-  //  (although you could investigate JS array maps yourself)
-  let sequence = [];
-  for (let n = 1; n <= endNumber; n = n + 1 /* n++ more common */) {
-    // demoing variable declaration and assignment on separate lines below
-    // although it'd be more typical in this case to do a one liner
-    let nextFizzbuzzEntry;
-    nextFizzbuzzEntry = toFizzbuzz(n);
-    // let nextFizzbuzzEntry = toFizzbuzz(n) // <-- what we would normally do
-    sequence.push(nextFizzbuzzEntry);
-  }
-  return sequence;
-}
+// function fizzbuzz(endNumber) {
+//   // Note: an array map would be preferred here, but we'll look at maps later
+//   //  (although you could investigate JS array maps yourself)
+//   let sequence = [];
+//   for (let n = 1; n <= endNumber; n = n + 1 /* n++ more common */) {
+//     // demoing variable declaration and assignment on separate lines below
+//     // although it'd be more typical in this case to do a one liner
+//     let nextFizzbuzzEntry;
+//     nextFizzbuzzEntry = toFizzbuzz(n);
+//     // let nextFizzbuzzEntry = toFizzbuzz(n) // <-- what we would normally do
+//     sequence.push(nextFizzbuzzEntry);
+//   }
+//   return sequence;
+// }
 
-function isFizz(n) {
-  return n % 3 === 0;
-}
+// function isFizz(n) {
+//   return n % 3 === 0;
+// }
 
-function isBuzz(n) {
+// function isBuzz(n) {
+//   return n % 5 === 0;
+// }
+
+// function isFizzbuzz(n) {
+//   return isFizz(n) && isBuzz(n);
+// }
+
+// function toFizzbuzz(n) {
+//   if (isFizzbuzz(n)) {
+//     return "FizzBuzz";
+//   } else if (isFizz(n)) {
+//     return "Fizz";
+//   } else if (isBuzz(n)) {
+//     return "Buzz";
+//   } else {
+//     return n;
+//   }
+// }
+
+/* Ignore the commented-out code below until Exercise 3 */
+let isBuzz = (n) => {
   return n % 5 === 0;
 }
 
-function isFizzbuzz(n) {
+let isFizz = (n) => {
+  return n % 3 === 0;
+}
+
+let isFizzbuzz = (n) => {
   return isFizz(n) && isBuzz(n);
 }
 
-function toFizzbuzz(n) {
+let toFizzbuzz = (n) => {
   if (isFizzbuzz(n)) {
-    return "FizzBuzz";
-  } else if (isFizz(n)) {
-    return "Fizz";
-  } else if (isBuzz(n)) {
-    return "Buzz";
-  } else {
+    return 'FizzBuzz';
+  }
+  else if (isFizz(n)) {
+    return 'Fizz';
+  }
+  else if (isBuzz(n)) {
+    return 'Buzz';
+  }
+  else {
     return n;
   }
 }
 
-/* Ignore the commented-out code below until Exercise 3 */
-// let isBuzz = (n) => {
-//   return n % 5 === 0;
-// }
-
+let fizzbuzz = (number) => {
+  let sequence = [];
+  for (let n = 1; n <= number; n++ ) {
+    let nextFizzbuzzEntry = toFizzbuzz(n);
+    sequence.push(nextFizzbuzzEntry);
+  }
+  return sequence;
+}
 /* Ignore the commented-out code below until Exercise 4 */
 // let isBuzz = (n) => n % 5 === 0;
 
